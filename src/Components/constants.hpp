c334cdada8a1a229a16382845f861ccba377d4dd
Valeri Petkov <mopchowork@gmail.com>
2026-06-12 13:37:08 +0300

    Add TEXTURES_FOLDER_PATH constant

--- src/Components/constants.hpp
+++ src/Components/constants.hpp
@@ -3,3 +3,4 @@
 
 std::filesystem::path GetExecutablePath();
 inline const std::filesystem::path EXECUTABLE_FOLDER_PATH = GetExecutablePath().parent_path();
+inline const std::filesystem::path TEXTURES_FOLDER_PATH = EXECUTABLE_FOLDER_PATH / "Textures";
